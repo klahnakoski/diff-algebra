f29049cebc528a252721d42e52b0f9c5bba02fd4
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-07-23 11:43:41 -0400

    better example

--- tests/resources/example_file.py
+++ tests/resources/example_file.py
@@ -5,7 +5,4 @@ a = time.time()
 b = "hello world"
 
 # SOME COMMENTS WILL HELP READER DEAL WITH THE COMPLEXITY
-if a < 1.5 * 1000 * 1000 * 1000:
-    print "the past"
-else:
-    print "the future"
+print "the future"
